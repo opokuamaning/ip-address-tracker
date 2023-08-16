52fa5d8d6074cd38f9d84e5437a64c99d7802f1f
Prince Opoku Amaning <105636490+opokuamaning@users.noreply.github.com>
2023-08-16 13:46:10 +0000

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import "leaflet/dist/leaflet.css";
-import Leaflet from "leaflet"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Leaflet from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import OsmProviders from "./OsmProviders";
 
 function Home() {
@@ -10,28 +10,35 @@ function Home() {
     iconUrl: require("leaflet/dist/images/marker-icon.png"),
     iconSize: [35, 45],
     iconAnchor: [17, 46],
-    popupAnchor: [0, -45]
-  })
+    popupAnchor: [0, -45],
+  });
   const [center, setCenter] = useState({
-    lat: 51.505,
-    lng: -0.09,
-  })
+    lat: 34.05223,
+    lng: -118.24368,
+  });
   const ZOOM_LEVEL = 13;
   const mapRef = useRef();
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("1.1.1.1");
   const [data, setData] = useState(null);
   const getIpInfoOnBtnClick = async () => {
     let url = `http://localhost:3080/ip-info/${inputValue}`;
     let response = await axios.get(url);
     console.log(response.data.result);
     setData(response.data.result);
-    const {
-      lat,
-      lng
-    } = response.data.result.location;
-    setCenter({ lat, lng })
+    const { lat, lng } = response.data.result.location;
+    setCenter({ lat, lng });
+  };
+  const getIpAndLocation = async () => {
+    let url = `http://localhost:3080/ip-info/${inputValue}`;
+    let response = await axios.get(url);
+    setData(response.data.result);
+    const { lat, lng } = response.data.result.location;
+    setCenter({ lat, lng });
   };
 
+  useEffect(() => {
+    getIpAndLocation();
+  }, []);
   return (
     <>
       <header>
@@ -58,13 +65,18 @@ function Home() {
           </div>
           <div className="location-section info-section-div">
             <p className="title">LOCATION</p>
-            <p className="description">{data?.location ? `${data?.location.region},  ${data?.location.postalCode}` : ""}</p>
+            <p className="description">
+              {data?.location
+                ? `${data?.location.region},  ${data?.location.postalCode}`
+                : ""}
+            </p>
           </div>
           <div className="timezone-section info-section-div">
             <p className="title">TIMEZONE</p>
 
-            <p className="description">{data?.location ? `UTC ${data?.location.timezone || ""}` : ""}</p>
-
+            <p className="description">
+              {data?.location ? `UTC ${data?.location.timezone || ""}` : ""}
+            </p>
           </div>
           <div className="isp-section">
             <p className="title">ISP</p>
@@ -73,19 +85,31 @@ function Home() {
         </div>
       </section>
       <section className="map-section">
-        <MapContainer
-          center={center}
-          zoom={ZOOM_LEVEL}
-          className="my-map"
-          ref={mapRef}
-        >
-          <TileLayer url={OsmProviders.maptiler.url} attribution={OsmProviders.maptiler.attribution} />
-          <Marker position={center} icon={markerIcon}>
-            <Popup>
-              <p>{data?.location ? `${data?.location.region},  Lat: ${data?.location.lat}, Lng: ${data?.location.lng}` : ""}</p>
-            </Popup>
-          </Marker>
-        </MapContainer>
+        {data?.location ? (
+          <MapContainer
+            center={[data?.location.lat, data?.location.lng]}
+            zoom={ZOOM_LEVEL}
+            className="my-map"
+            ref={mapRef}
+          >
+            <TileLayer
+              url={OsmProviders.maptiler.url}
+              attribution={OsmProviders.maptiler.attribution}
+            />
+            <Marker
+              position={[data.location.lat, data.location.lng]}
+              icon={markerIcon}
+            >
+              <Popup>
+                <p>
+                  {data?.location
+                    ? `${data?.location.region},  Lat: ${data?.location.lat}, Lng: ${data?.location.lng}`
+                    : ""}
+                </p>
+              </Popup>
+            </Marker>
+          </MapContainer>
+        ) : null}
       </section>
     </>
   );
